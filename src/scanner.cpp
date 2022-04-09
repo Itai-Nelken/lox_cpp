2ee6101276788700b3210fbb62183d933893bf29
Itai-Nelken <itainelken@gmail.com>
2022-04-09 18:18:23 +0300

    Scanner: fix < and > being swapped
    Signed-off-by: Itai-Nelken <itainelken@gmail.com>

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -195,8 +195,8 @@ Token Scanner::scanToken() {
         case '*': return makeToken(TokenType::STAR);
         case '!': return makeToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG);
         case '=': return makeToken(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL);
-        case '<': return makeToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
-        case '>': return makeToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
+        case '<': return makeToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
+        case '>': return makeToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
         case '"':return string();
         default:
             break;
